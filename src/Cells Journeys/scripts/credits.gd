87d4c0e4ada43e8cec0afa108b59c417a036afed
edu-porto <eduardo.porto@sou.inteli.edu.br>
2022-03-18 11:35:11 -0300

    Deploy 18/03/22
    Add o source e executaveis

--- src/Cells Journeys/scripts/credits.gd
+++
@@ -1,4 +0,0 @@
-extends Control
-
-func _on_back_pressed():
-	get_tree().change_scene("res://scenes/main menu.tscn")
--- src/Cells Journeys/scripts/script_global.gd
+++
@@ -1,8 +0,0 @@
-extends Node
-
-var tenis = 1
-var vilaoDied = false
-var posicaoVilao = Vector2()
-var dano = false
-var vilaoHit = false
-
